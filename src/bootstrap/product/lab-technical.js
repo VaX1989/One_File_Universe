669dafc44ec737a82d216d5557fc95167b5e2e0a
VaX1989 <Pietrocorona89@gmail.com>
2026-09-04 10:45:34 +0200

    v0.8 seam: add Lab technical extension point

---
+++ src/bootstrap/product/lab-technical.js
@@ -0,0 +1,5 @@
+(function(root){
+'use strict';
+const O=root.OFU=root.OFU||{};
+O.v08LabTechnical=O.v08LabTechnical||Object.freeze({seamVersion:1});
+})(typeof globalThis!=='undefined'?globalThis:this);
