f994ea25f9fb1c43c6829b1fca4574a25475dc3a
VaX1989 <Pietrocorona89@gmail.com>
2026-09-04 10:45:26 +0200

    v0.8 seam: add Inspector product extension point

---
+++ src/bootstrap/product/inspector-product.js
@@ -0,0 +1,5 @@
+(function(root){
+'use strict';
+const O=root.OFU=root.OFU||{};
+O.v08InspectorProduct=O.v08InspectorProduct||Object.freeze({seamVersion:1});
+})(typeof globalThis!=='undefined'?globalThis:this);
