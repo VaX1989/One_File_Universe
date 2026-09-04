5b1ea4bcb949fa83411c21f8f78ae2b849822b86
VaX1989 <Pietrocorona89@gmail.com>
2026-09-04 10:45:19 +0200

    v0.8 seam: add Explore navigation extension point

---
+++ src/bootstrap/product/explore-navigation.js
@@ -0,0 +1,5 @@
+(function(root){
+'use strict';
+const O=root.OFU=root.OFU||{};
+O.v08ExploreNavigation=O.v08ExploreNavigation||Object.freeze({seamVersion:1});
+})(typeof globalThis!=='undefined'?globalThis:this);
